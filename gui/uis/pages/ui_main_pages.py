f67e423139bc89e161dcffc504190fbf60da9aa1
Cairoknox <72080616+Cairoknox@users.noreply.github.com>
2022-05-03 13:58:04 +0200

    Upload (3)

---
+++ gui/uis/pages/ui_main_pages.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_pagesSBXhMU.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from qt_core import *
+
+
+class Ui_MainPages(object):
+    def setupUi(self, MainPages):
+        if not MainPages.objectName():
+            MainPages.setObjectName(u"MainPages")
+        MainPages.resize(860, 600)
+        self.main_pages_layout = QVBoxLayout(MainPages)
+        self.main_pages_layout.setSpacing(0)
+        self.main_pages_layout.setObjectName(u"main_pages_layout")
+        self.main_pages_layout.setContentsMargins(5, 5, 5, 5)
+        self.pages = QStackedWidget(MainPages)
+        self.pages.setObjectName(u"pages")
+        self.pages.setStyleSheet(u"font-size: 14pt;background: lightblue;")
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.page_1.setStyleSheet(u"font-size: 14pt;background: lightgreen;")
+        self.page_1_layout = QVBoxLayout(self.page_1)
+        self.page_1_layout.setSpacing(5)
+        self.page_1_layout.setObjectName(u"page_1_layout")
+        self.page_1_layout.setContentsMargins(5, 5, 5, 5)
+        self.pages.addWidget(self.page_1)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.page_2.setStyleSheet(u"font-size: 14pt;background: orange;")
+        self.page_2_layout = QVBoxLayout(self.page_2)
+        self.page_2_layout.setSpacing(5)
+        self.page_2_layout.setObjectName(u"page_2_layout")
+        self.page_2_layout.setContentsMargins(5, 5, 5, 5)
+        self.pages.addWidget(self.page_2)
+        self.page_3 = QWidget()
+        self.page_3.setObjectName(u"page_3")
+        self.page_3.setStyleSheet(u"QFrame {\n"
+"	font-size: 16pt;\n"
+"}")
+        self.page_3_layout = QVBoxLayout(self.page_3)
+        self.page_3_layout.setObjectName(u"page_3_layout")
+        self.pages.addWidget(self.page_3)
+
+        self.main_pages_layout.addWidget(self.pages)
+
+
+        self.retranslateUi(MainPages)
+
+        self.pages.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainPages)
+    # setupUi
+
+    def retranslateUi(self, MainPages):
+        MainPages.setWindowTitle(QCoreApplication.translate("MainPages", u"Form", None))
+    # retranslateUi
+
